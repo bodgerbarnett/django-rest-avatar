2dac15c5f998f612da38b7b225b977b151ca8163
Simon Barnett <sbarnett@acresofspace.co.uk>
2021-02-22 16:40:50 +0100

    Bumped version

---
+++ tests/test_serializers.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from rest_avatar import serializers
+
+
+User = get_user_model()
+
+
+class TestSerializers(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('john@beatles.com', 'secret')
+
+    def test_validate_file_too_big(self):
+        # TODO how to upload images?
+        serializer = serializers.AvatarSerializer(data=data)
+        assert not serializer.is_valid()
+
+    def tearDown(self):
+        pass

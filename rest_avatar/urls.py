103f04af0e953e6e9205489745509b009c480f33
Simon Barnett <sbarnett@acresofspace.co.uk>
2021-02-22 14:24:16 +0100

    Some changes

--- rest_avatar/urls.py
+++ rest_avatar/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 from .views import AvatarViewSet
 
 
+app_name = 'rest_avatar'
+
 router = routers.DefaultRouter()
 router.register('', AvatarViewSet, basename='avatar')
 
